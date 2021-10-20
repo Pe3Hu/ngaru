4183236519aa0b6aad1622f43254bdca61fde202
Pe3Hu <34793005+Pe3Hu@users.noreply.github.com>
2021-10-20 17:00:01 +0300

    update version 0.002
    1. stencil added

--- player/Player.gd
+++ player/Player.gd
@@ -1,8 +1,7 @@
 extends Node
 
-var x_start = 8
-var y_start = 8
-var offset = 52
+var vial_offset = 56
+var panel_offset = 12
 
 var hand_size = 5
 var deck_size = 10
@@ -18,10 +17,43 @@ var charge
 var rng = RandomNumberGenerator.new()
 	
 func save():
-	var save_data = File.new()
-	
-	save_data.open(path_vial_types, File.WRITE)
-	save_data.store_line(to_json(charge))
+	var save_data = File.new()	
+	var dictionary = {
+		"2":{
+			"binary": [],
+			"int": []	
+			}	
+		}
+		
+	var l = 2 * 5 - 1	
+	for i in 2:
+		for j in 5:
+			if(j > 0):
+				var txt = "0000000000"
+				txt[l-i*5] = "1"
+				txt[l-i*5-j] = "1"
+				dictionary["2"]["binary"].append(txt)
+				
+		for j in 5:
+			if(j == 1) or (j == 2):		
+				var txt = "0000000000"			
+				txt[l-i*5-j] = "1"		
+				txt[l-(i+1)*5+j] = "1"
+				dictionary["2"]["binary"].append(txt)
+				
+	for txt in dictionary["2"]["binary"]:		
+		var n = 0
+		var d = 512
+		
+		for j in txt:			
+			if(j == "1"):
+				n+=d
+			d/=2
+		
+		dictionary["2"]["int"].append(n)	
+		
+	save_data.open("res://stencil/stencil_types.json", File.WRITE)
+	save_data.store_line(to_json(dictionary))
 	save_data.close()
 	
 func load_vial_types():
@@ -44,15 +76,16 @@ func init_vials():
 		rng.randomize()	
 		var index = floor(rng.randf_range(0, vial_types.size()))
 		var charge = vial_types[index]
-		vial.set_charge(vial_types[index])
+		vial.set_charge(charge)
 		add_child(vial)
 		draw_deck.append(vial)
-	
-	print(draw_deck)
 
-func grid_to_pixel(index):
-	var x = x_start + offset * 0;	
-	var y = y_start + offset * index;	
+func grid_to_pixel(index):	
+	var parent = get_parent()
+	var x_start = parent.get_rect().position.x
+	var y_start = parent.get_rect().position.y
+	var x = x_start + panel_offset;	
+	var y = y_start + panel_offset + vial_offset * index;	
 	return Vector2(x, y);
 
 func fill_hand():
@@ -63,7 +96,6 @@ func fill_hand():
 		var index = current_hand.find(vial,0)
 		vial.set_position(grid_to_pixel(index))
 		vial.set_visible(true) 
-	print(current_hand)	
 
 func draw_from_deck():
 	if(draw_deck.size() == 0):
@@ -73,9 +105,8 @@ func draw_from_deck():
 		discard_deck = []
 		
 	current_hand.append(draw_deck.pop_back())
-	
 		
 func _ready():
 	init_vials()
 	fill_hand()
-
+	save()
--- vial/Vial.gd
+++ vial/Vial.gd
@@ -3,13 +3,13 @@ extends Node
 func set_charge(charge):		
 	for first_key in charge:		
 		for second_key in charge[first_key]:	
-			get_node("charge/"+first_key+"/"+second_key).text = String(charge[first_key][second_key])
+			get_node("container/charge/"+first_key+"/"+second_key).text = String(charge[first_key][second_key])
 
 func set_position(vec):	
-	get_node("charge").rect_position = vec
+	get_node("container").rect_position = vec
 	
 func set_visible(flag):	
-	get_node("charge").visible = flag
+	get_node("container").visible = flag
 
 func _ready():
 	pass # Replace with function body.
